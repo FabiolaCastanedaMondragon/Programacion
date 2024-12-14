857e458835db41a83e3209936f22575bff19a747
FabiolaCastanedaMondragon <162269244+FabiolaCastanedaMondragon@users.noreply.github.com>
2024-12-13 19:11:24 -0600

    Add files via upload

---
+++ Ejercicio1.php
@@ -0,0 +1,10 @@
+<html>
+
+<head></head>
+
+<body>
+    <?php
+    echo "Bienvenidos al Tesji :)";
+    ?>
+<body>
+</html>        
---
+++ Ejercicio2.php
@@ -0,0 +1,25 @@
+<body>
+
+<?php
+
+$edad=20;
+$peso=90.25;
+$nombre="Juan";
+$existe=true;
+echo "Variable integer";
+echo $edad;
+echo "<br>";
+echo "Variable double";
+echo $peso;
+echo "<br>";
+echo "Variable string";
+echo $nombre;
+echo "<br>";
+echo "Variable boolean";
+echo $existe;
+
+?>
+
+</body>
+
+</html>
